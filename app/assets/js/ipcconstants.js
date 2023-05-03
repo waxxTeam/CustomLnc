a5d677c9116f9f269af9fee7d8236bc588ec6113
goooom <64928263+joooom@users.noreply.github.com>
2023-05-03 18:13:51 -0300

    0.1.0

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '0db4310c-0046-4fca-a767-df2f89e7fb9d'
 // SEE NOTE ABOVE.
 
 
